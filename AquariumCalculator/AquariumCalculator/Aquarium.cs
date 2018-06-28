aaa434cd0a6b246b0a6e41687adb8144faf2da8c
nmalocic <coderns@gmail.com>
2018-06-28 20:02:17 +0200

    Initial implemenatation for aquarium and calcualting Volume in liters and size of all sides

---
+++ AquariumCalculator/AquariumCalculator/Aquarium.cs
@@ -0,0 +1,33 @@
+﻿namespace AquariumCalculatorTests
+{
+    internal class Aquarium
+    {
+        private int length;
+        private int width;
+        private int height;
+        private readonly int _toLiters = 1000;
+
+        public Aquarium(int length, int width, int height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        internal int GetVolume()
+        {
+            return length * width * height / _toLiters;
+        }
+
+        internal int GetGlassSize()
+        {
+            return 2 * WidthArea + 2 * LengthArea + BaseArea;
+        }
+
+        private int WidthArea => width * height;
+
+        private int LengthArea => length * height;
+
+        private int BaseArea => width * height;
+    }
+}
---
+++ AquariumCalculator/AquariumCalculator/AquariumSizeTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+
+namespace AquariumCalculatorTests
+{
+    public class AquariumSizeTests
+    {
+        [Fact]
+        public void Given_Aquarium_Size_Returns_Volume()
+        {
+            int width = 50;
+            int length = 50;
+            int height = 50;
+
+            Aquarium aq = new Aquarium(length, width, height);
+
+            var volume = aq.GetVolume();
+
+            Assert.Equal(125, volume);
+        }
+
+        [Fact]
+        public void Given_Aquarium_Size_Returns_GlassArea()
+        {
+            int width = 50;
+            int length = 50;
+            int height = 50;
+
+            Aquarium aq = new Aquarium(length, width, height);
+            int glassSize = aq.GetGlassSize();
+
+            Assert.Equal(12500, glassSize);
+        }
+    }
+}
