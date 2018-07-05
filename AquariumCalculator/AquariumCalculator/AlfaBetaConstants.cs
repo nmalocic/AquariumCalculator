9988c193bbc39b0c833e0fd6a23c67c17a742cc4
Nemanja Malocic <coderns@hotmail.com>
2018-07-05 13:58:19 +0200

    Fixed identation

--- AquariumCalculator/AquariumCalculator/AlfaBetaConstants.cs
+++ AquariumCalculator/AquariumCalculator/AlfaBetaConstants.cs
@@ -4,7 +4,8 @@ namespace AquariumCalculatorTests
 {
     public class AlfaBetaConstants
     {
-        public AlfaBetaConstants(double ratio, double alpha, double beta)        {
+        public AlfaBetaConstants(double ratio, double alpha, double beta)
+        {
             if(alpha < 0 || beta < 0 || ratio < 0)
                 throw new ArgumentException("Alpha and beta must be greater than zero.");
 
